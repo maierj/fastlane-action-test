1e26da9f18727118435e541a282c448742f6e31f
Jonas Maier (Private) <jonasmaierprivate@adabays-MBP-2.fritz.box>
2019-10-07 01:12:50 +0200

    Add second test step in workflow

---
+++ FastlaneActionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneActionTest
+//
+//  Created by Jonas Maier (Private) on 06.10.19.
+//  Copyright © 2019 Jonas Maier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
